94c234d7f7d2c6421af945727bd2f767abc1dffc
Enzo Nio <enzonio@hotmail.com>
2020-03-11 12:59:27 +0100

    More verbose, ${1} is resolution

--- fsl_reorient_processed.sh
+++ fsl_reorient_processed.sh
@@ -5,13 +5,41 @@ find . -type f -name "*.nii*"
 for imagename in $(find . -type f -name "*.nii*"); do
 
 	echo "Start image reorientation for ${imagename}"
+	
+	imagename=${imagename##*/}
+	imagename_reoriented="${imagename%%.*}_reoriented.nii.gz"
 
-	fslorient -setqformcode 1 $imagename
-	fslorient -setqform 0.05 0 0 0 0 -0.05 0 0 0 0 0.05 0 0 0 0 1 $imagename
-	fslorient -setsformcode 0 $imagename
-	fslreorient2std $imagename
+	sudo cp $imagename $imagename_reoriented
+	sudo chmod 777 $imagename_reoriented
 
+	
+	
+	echo
+	echo "header before"
 	fslhd $imagename
-	#~ fsleyes $imagename
+	
+	fslorient -setqformcode 1 $imagename_reoriented
+	fslorient -setqform ${1} 0 0 0 0 -${1} 0 0 0 0 ${1} 0 0 0 0 1 $imagename_reoriented
+	fslorient -setsformcode 0 $imagename_reoriented
+	
+	echo
+	echo "header after"
+	fslhd $imagename_reoriented
+	
+	
+	
+	echo
+	echo "reorientation matrix to MNI before"
+	fslreorient2std $imagename_reoriented
+	
+	fslreorient2std $imagename_reoriented $imagename_reoriented
+	
+	echo
+	echo "reorientation matrix to MNI after"
+	fslreorient2std $imagename_reoriented
+	
+	
+
+	fsleyes $imagename_reoriented
 
 done
