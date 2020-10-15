2cdebd5c3b89b26f4e53d1fd97ba38c63dfadcd5
EnzoNio <enzonio@hotmail.com>
2020-10-15 14:52:52 +0200

    shape dissimilarity update
    all subjects, inverse flirt in reference space

--- shape_dissimilarity.py
+++ shape_dissimilarity.py
@@ -5,44 +5,60 @@ import numpy as np
 import os
 import glob
 
+# Define paths
 data_path = os.path.join('Data', 'Mouse', 'Processed')
+reference_path = os.path.join('Data', 'Mouse', 'Reference')
 data_path_list = glob.glob(os.path.join(data_path, '*'))
+ref_path = os.path.join(reference_path, 'annotation_25_reoriented_flirted.nii.gz')
+ref_image = nib.load(ref_path)
+
 for Path in data_path_list:
     subject = Path.split(os.sep)[-1]
+    print(subject)
 
+    # Define subject specific paths
     or_path = os.path.join(Path, subject + '.nii.gz')
     flirtRigid_path = os.path.join(Path, subject + '_flirtRigid.mat')
     flirt_path = os.path.join(Path, subject + '_flirt.mat')
+    invflirt_path = os.path.join(Path, subject + '_invflirt.mat')
     defField_path = os.path.join(Path, subject + '_flirt_defField.nii.gz')
     defField_magnitude_path = os.path.join(Path, subject + '_flirt_defField_magnitude.nii.gz')
 
     or_image = nib.load(or_path)
 
-    with open('Data/Mouse/Processed/WT_50/WT_50_flirt.mat', 'r') as f:
+    with open(flirt_path, 'r') as f:
         txt = f.read()
         flirt = np.array([[float(num) for num in item.split()] for item in txt.split('\n')[:-1]])
     print(flirt)
+    with open(invflirt_path, 'r') as f:
+        txt = f.read()
+        invflirt = np.array([[float(num) for num in item.split()] for item in txt.split('\n')[:-1]])
+    print(invflirt)
 
-defField = imageFLIRT2defField(or_image, flirt)
-defField_image = nib.Nifti1Image(defField, or_image.affine)
-nib.save(defField_image, defField_path)
-defField_magnitude = np.sqrt(np.sum(np.power(defField, 2), axis=3))
-defField_magnitude_image = nib.Nifti1Image(defField_magnitude, or_image.affine)
-nib.save(defField_magnitude_image, defField_magnitude_path)
+    # Calculate inverse flirt and check whether it matches with read inverse flirt
 
-# Load SyN of WT_50 and get inverse field
-ref_path = 'Data/Mouse/Reference/annotation_25_reoriented_flirted_synned.nii.gz'
-defField_path = 'Data/Mouse/Processed/WT_50/WT_50_flirted_syn_defField.nii.gz'
-defField_magnitude_path = 'Data/Mouse/Processed/WT_50/WT_50_flirted_syn_defField_magnitude.nii.gz'
-ref_image = nib.load(ref_path)
-with open('Data/Mouse/Processed/WT_50/WT_50_flirted_syn.pickle.gz', 'rb') as f:
-    [mapping, metric, level_iters, sdr] = load(f, compression='gzip')
+    #
+    defField = imageFLIRT2defField(ref_image, invflirt)
+    defField_image = nib.Nifti1Image(defField, ref_image.affine)
+    nib.save(defField_image, defField_path)
+    defField_magnitude = np.sqrt(np.sum(np.power(defField, 2), axis=3))
+    defField_magnitude_image = nib.Nifti1Image(defField_magnitude, ref_image.affine)
+    nib.save(defField_magnitude_image, defField_magnitude_path)
 
-defField = mapping.get_backward_field()
-del mapping, metric, level_iters, sdr
 
-defField_image = nib.Nifti1Image(defField, ref_image.affine)
-nib.save(defField_image, defField_path)
-defField_magnitude = np.sqrt(np.sum(np.power(defField, 2), axis=3))
-defField_magnitude_image = nib.Nifti1Image(defField_magnitude, ref_image.affine)
-nib.save(defField_magnitude_image, defField_magnitude_path)
+
+    # Load SyN of subject and get inverse field
+    defField_path = os.path.join(Path, subject + '_flirted_syn_defField.nii.gz')
+    defField_magnitude_path = os.path.join(Path, subject + '_flirted_syn_defField_magnitude.nii.gz')
+    syn_path = os.path.join(Path, subject + '_flirted_syn.pickle.gz')
+    with open(syn_path, 'rb') as f:
+        [mapping, metric, level_iters, sdr] = load(f, compression='gzip')
+
+    defField = mapping.get_backward_field()
+    del mapping, metric, level_iters, sdr
+
+    defField_image = nib.Nifti1Image(defField, ref_image.affine)
+    nib.save(defField_image, defField_path)
+    defField_magnitude = np.sqrt(np.sum(np.power(defField, 2), axis=3))
+    defField_magnitude_image = nib.Nifti1Image(defField_magnitude, ref_image.affine)
+    nib.save(defField_magnitude_image, defField_magnitude_path)
